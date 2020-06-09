2703f86e86bb58ee2fd8c4d472cbd77212cefa5c
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-06-09 18:50:44 +0900

    useReducerを使って見た

--- booklist/src/contexts/BookContext.jsx
+++ booklist/src/contexts/BookContext.jsx
@@ -1,20 +1,12 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useReducer } from "react";
+import { bookReducer } from "../reducers/bookReducer";
 
 export const BookContext = createContext();
 
 export default function BookContextProvider(props) {
-  const [books, setBooks] = useState([
-    { title: "name of wind", author: "takashi", id: 1 },
-    { title: "name of kaito", author: "takashi", id: 2 }
-  ]);
-  const addBook = (title, author) => {
-    setBooks([...books, { title, author, id: Date.now() }]);
-  };
-  const removeBook = (id) => {
-    setBooks(books.filter((book) => book.id !== id));
-  };
+  const [books, dispatch] = useReducer(bookReducer, []);
   return (
-    <BookContext.Provider value={{ books, addBook, removeBook }}>
+    <BookContext.Provider value={{ books, dispatch }}>
       {props.children}
     </BookContext.Provider>
   );
